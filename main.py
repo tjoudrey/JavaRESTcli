0a81185264836c70b01e5e48130d04e15b18c552
tyler <tylerjoudrey@gmail.com>
2019-10-13 04:59:02 -0300

    file generator flags added

--- main.py
+++ main.py
@@ -7,18 +7,47 @@ import services.fileWriterService as fileWriter
 
 
 @click.command()
-@click.argument('project_name')
-@click.argument('database_name')
-@click.option('--greeting', '-g')
-def main(project_name, database_name,  greeting):
-    click.echo("Making {} ...".format(project_name))
-    directoryService.make_file_structure(project_name)
+@click.option('--generate', '-g', is_flag=True, help="Generate and build a spring server")
+@click.option('--service', '-s', nargs=2, help="Generate a service class specifying the project name and service name")
+@click.option('--repo', '-r', nargs=2, help="Generate a repository interface specifying the project name and model name")
+@click.option('--model', '-m', nargs=3, help="Generate a model specifying the project name, model name and db schema "
+                                             "name")
+@click.option('--controller', '-c',nargs=2, help="Generate a api controller specifying the project name and model name")
+def main(generate, service, repo, model, controller):
+    if generate:
+        project_name = click.prompt("Input project name")
+        database_name = click.prompt("Input database name")
+        database_user = click.prompt("Input database username")
+        database_pass = click.prompt("Input database password")
 
-    fileWriter.write_pom(project_name)
-    fileWriter.write_application_properties(project_name, database_name)
-    fileWriter.write_application_java(project_name)
+        """a cli to generate java REST apis for postgreSQL"""
+        click.echo("Making {} ...".format(project_name))
+        directoryService.make_file_structure(project_name)
 
-    os.system("cd "+project_name+"/; mvn install")
+        fileWriter.write_pom(project_name)
+        fileWriter.write_application_properties(project_name, database_name)
+        fileWriter.write_application_java(project_name)
+
+        os.system("cd "+project_name+"/; mvn install")
+
+    elif model:
+        print(model)
+        fileWriter.write_model(model[0], model[1], model[2])
+
+    elif repo:
+        fileWriter.write_repo(model[0], model[1])
+
+    elif controller:
+        fileWriter.write_controller(controller[0], controller[1])
+
+    elif service:
+        fileWriter.write_service(service[0], service[1])
+
+    else:
+        ctx = click.get_current_context()
+        click.echo(ctx.get_help())
+        ctx.exit()
+
 
 if __name__ == "__main__":
     main()
--- services/fileWriterService.py
+++ services/fileWriterService.py
@@ -104,3 +104,124 @@ public class Application {\n\
     }\n\
 }"
     )
+
+
+def write_model(project_name, model_name, schema):
+    current_path = os.getcwd()
+    model = open(current_path+"/"+model_name.title()+".java", "w+")
+    model.write(
+"com."+project_name+".models;\n\
+import javax.persistence.*;\n\
+@Entity\n\
+@Table(name = \""+model_name+"\", schema = \""+schema+"\")\n\
+public class "+model_name+" {\n\
+\n\
+    @Id\n\
+    @GeneratedValue(strategy = GenerationType.AUTO)\n\
+    @Column(name=\"id\", unique = true)\n\
+    private long id;\n\
+\n\
+     @Column(name=\"created_on\")\n\
+    private ZonedDateTime created_on;\n\
+\n\
+    public "+model_name+"() {\n\
+    this.setCreated_on(ZonedDateTime.now());\n\
+    }\n\
+\n\
+    public long getId() {\n\
+        return id;\n\
+    }\n\
+\n\
+    public void setId(long id) {\n\
+        this.id = id;\n\
+    }\n\
+\n\
+    public ZonedDateTime getCreated_on() {\n\
+        return created_on;\n\
+    }\n\
+"
+            )
+
+
+def write_repo(project_name, model_name):
+    current_path = os.getcwd()
+    model = open(current_path + "/" + model_name.title()+"Repository.java", "w+")
+    model.write("\n\
+package com."+project_name+".repositories;\n\
+\n\
+import com."+project_name+".models."+model_name.title()+";\n\
+import org.springframework.data.jpa.repository.JpaRepository;\n\
+import org.springframework.stereotype.Repository;\n\
+\n\
+@Repository\n\
+public interface ""Repository extends JpaRepository<"+model_name.title()+", Long> {\n\
+}")
+
+
+def write_controller(project_name, model_name):
+    current_path = os.getcwd()
+    controller = open(current_path + "/" + model_name.title() + "Controller.java", "w+")
+    controller.write("\n\
+package com."+project_name+".controllers;\n\
+                     \n\
+import com.forte."+project_name+".models."+model_name+";\n\
+import com.forte."+project_name+".repositories."+model_name+"Repository;\n\
+import org.springframework.beans.factory.annotation.Autowired;\n\
+import org.springframework.web.bind.annotation.*;\n\
+    \n\
+@RestController\n\
+@RequestMapping(\"/api\")\n\
+public class "+model_name.title()+"Controller {\n\
+    \n\
+    @Autowired\n\
+    "+model_name.title()+"Repository "+model_name.lower()+"Repository;\n\
+    \n\
+    \n\
+    @GetMapping(\"/"+model_name.lower()+"\")\n\
+    public List<"+model_name.title()+"> getAll"+model_name.title()+"() {\n\
+        return "+model_name.lower()+"Repository.findAll();\n\
+    }\n\
+        \n\
+    @GetMapping(\"/"+model_name.lower()+"/{id}\")\n\
+    public "+model_name.title()+" get"+model_name.title()+"(@PathVariable(value = \"id\") Long id){\n\
+        "+model_name.title()+" "+model_name.lower()+" = new "+model_name.title()+"();\n\
+        return "+model_name.lower()+"Repository.findById(id).orElse("+model_name.lower()+");\n\
+    }\n\
+            \n\
+    @PostMapping(\"/"+model_name.lower()+"\")\n\
+    public void set"+model_name.title()+"() throws MessagingException {\n\
+        "+model_name.title()+" "+model_name.lower()+" = new "+model_name.title()+"();\n\
+        "+model_name.lower()+"Repository.save("+model_name.lower()+");\n\
+    }\n\
+    \n\
+    @DeleteMapping(\"/"+model_name.title()+"\")\n\
+    public void delete"+model_name.title()+"(Long id) {\n\
+        "+model_name.lower()+"Repository.deleteById(id);\n\
+    }\n\
+}\n\
+    ")
+
+
+def write_service(project_name, service_name):
+    current_path = os.getcwd()
+    service = open(current_path + "/" + service_name.title() + "Controller.java", "w+")
+    service.write("\n\
+package com."+project_name+".services;\n\
+\n\
+public class "+service_name.title()+"Service {\n\
+private static "+service_name.title()+"Service INSTANCE;\n\
+    \n\
+    private "+service_name.title()+"Service() {\n\
+    }\n\
+    \n\
+    public static "+service_name.title()+""+service_name.title()+" getInstance(){\n\
+    if (INSTANCE == null) {\n\
+        INSTANCE = new "+service_name.title()+"Service();\n\
+    }\n\
+    return INSTANCE;\n\
+    }\n\
+                                                "
+
+
+        )
+
